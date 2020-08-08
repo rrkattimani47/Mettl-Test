80e27f31370cad5593fe09ed37ddca1417ccbc64
Rashmi Kattimani <rrkattimani47@gmail.com>
2020-08-08 11:03:22 +0530

    Create isOdd.java

---
+++ isOdd.java
@@ -0,0 +1,23 @@
+Link : https://tests.mettl.com/authenticateKey/dbdac2a9
+Using Java 7
+
+import java.io.*;
+ import  java.util.*;
+
+  //Read only region
+
+  class UserMainCode {
+ public int isOdd(int input1){
+ //Read only region end
+ //Write code here....
+ 
+ if(input1 % 2 !=0)
+		{
+			return 2;
+		}
+		else{
+			return 1;
+		}
+		
+	}
+}
