37ac2dbb2c7b8393c84946c4e4b1de4034cf88d5
Rashmi Kattimani <rrkattimani47@gmail.com>
2020-08-08 10:52:47 +0530

    Create isEven.java

---
+++ isEven.java
@@ -0,0 +1,25 @@
+Link: https://tests.mettl.com/authenticateKey/2bd025dc
+Using Java 7
+
+
+import java.io.*;
+import  java.util.*;
+
+//Read only region
+
+class UserMainCode {
+public int isEven(int input1){
+//Read only region end
+//Write code here....
+
+
+if(input1 %2 ==0)
+	{
+		return 2;
+	}
+	else {
+		return 1;
+	}
+	}
+}
+
