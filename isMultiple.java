a7ea58d171fe9d9050bf0aa2f8defcde99b94ca0
Rashmi Kattimani <rrkattimani47@gmail.com>
2020-08-09 20:06:38 +0530

    Create isMultiple.java

---
+++ isMultiple.java
@@ -0,0 +1,28 @@
+Link: https://tests.mettl.com/authenticateKey/36c4ef58
+Using Java 7
+
+import java.io.*;
+import  java.util.*;
+
+//Read only region
+
+class UserMainCode {
+public int lastDigitOf(int input1){
+//Read only region end
+
+// Write code here...
+	if(input1==0 || input2 ==0)
+		{
+			return 3;
+		}
+	
+		if(input1 % input2 ==0){
+			return 2;
+		}
+		
+		else 
+		{
+			return 1;
+		}
+	}
+}
