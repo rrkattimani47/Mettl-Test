d1e2495bdd52bcf11a71a90577f414a2ca7faea2
Rashmi Kattimani <rrkattimani47@gmail.com>
2020-08-09 17:11:44 +0530

    Create addLastDigits.java

---
+++ addLastDigits.java
@@ -0,0 +1,30 @@
+Link: https://tests.mettl.com/authenticateKey/783a1fcf
+Using Java 7
+
+import java.io.*;
+import  java.util.*;
+
+//Read only region
+
+class UserMainCode {
+public int lastDigitOf(int input1){
+//Read only region end
+
+// Write code here...
+		int p,q,sum;
+		if(input1<0 ){
+			input1=input1 * -1;
+		
+		}
+		if(input2<0){
+	input2=input2* -1;
+
+		}
+		input1=input1 %10;
+		input2=input2 %10;
+		
+		sum= input1+input2 ;
+	return sum;
+		
+	}
+}
