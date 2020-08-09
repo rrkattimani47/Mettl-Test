c689d679b40ba5b81950fdb7fc40f72583e7db7c
Rashmi Kattimani <rrkattimani47@gmail.com>
2020-08-09 16:13:00 +0530

    Create lastDigitOf.java

---
+++ lastDigitOf.java
@@ -0,0 +1,14 @@
+Link: https://tests.mettl.com/authenticateKey/454f012b
+Using Java 7
+
+
+
+// Write code here...
+		int n;
+		if(input1<0){
+			input1=input1 * -1;
+		}
+    n=input1%10;
+    return n;
+	}
+}
