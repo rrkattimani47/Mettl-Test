3f90a5a9c98c1aabdc89319724e0d2884c57391f
Rashmi Kattimani <rrkattimani47@gmail.com>
2020-08-09 20:50:24 +0530

    Create countOdds.java

---
+++ countOdds.java
@@ -0,0 +1,34 @@
+Link: https://tests.mettl.com/authenticateKey/67147bd5
+Using Java 7
+
+import java.io.*;
+import  java.util.*;
+
+//Read only region
+
+class UserMainCode {
+public int countOdds(int input1){
+//Read only region end
+// Write code here...
+		int count=0;
+		if(input1 %2!=0){
+			count++;
+		}
+		if(input2%2!=0){
+			count++;
+		}
+		if(input3%2 !=0){
+			count++;
+
+		}
+		if(input4 % 2 != 0){
+			count++;
+
+		}
+		if(input5 %2 !=0){
+			count++;
+		}
+
+			return count++;
+	}
+}
