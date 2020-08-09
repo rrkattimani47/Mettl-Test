1fd780d1d26f21bb9ead9c68021f80e2229b61b1
Rashmi Kattimani <rrkattimani47@gmail.com>
2020-08-09 20:14:24 +0530

    Create countEvens.java

---
+++ countEvens.java
@@ -0,0 +1,35 @@
+Link: https://tests.mettl.com/authenticateKey/8edbe922
+Using Java 7
+
+import java.io.*;
+import  java.util.*;
+
+//Read only region
+
+class UserMainCode {
+public int lastDigitOf(int input1){
+//Read only region end
+
+// Write code here...
+		int count=0;
+		if(input1 %2==0){
+			count++;
+		}
+		if(input2%2==0){
+			count++;
+		}
+		if(input3%2 ==0){
+			count++;
+
+		}
+		if(input4 % 2== 0){
+			count++;
+
+		}
+		if(input5 %2 ==0){
+			count++;
+		}
+
+			return count++;
+	}
+}
