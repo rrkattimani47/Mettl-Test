d9a1a633a100245471838efec02b55e7aae9a8d4
Rashmi Kattimani <rrkattimani47@gmail.com>
2020-08-09 16:16:56 +0530

    Update lastDigitOf.java

--- lastDigitOf.java
+++ lastDigitOf.java
@@ -1,13 +1,21 @@
 Link: https://tests.mettl.com/authenticateKey/454f012b
 Using Java 7
 
+import java.io.*;
+import  java.util.*;
+
+//Read only region
+
+class UserMainCode {
+public int lastDigitOf(int input1){
+//Read only region end
 
 
 // Write code here...
-		int n;
-		if(input1<0){
-			input1=input1 * -1;
-		}
+int n;
+if(input1<0){
+input1=input1 * -1;
+	}
     n=input1%10;
     return n;
 	}
